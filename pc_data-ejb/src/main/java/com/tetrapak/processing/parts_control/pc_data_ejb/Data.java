fc4d1b413e930f9a078d0e266f193aa80ffafccc
lmpanalytics2 <lmpconsulting@hotmail.com>
2017-11-19 18:09:39 +0100

    add ejb remote data interface, add business method, change index.xhtml 	- add web.xml file

---
+++ pc_data-ejb/src/main/java/com/tetrapak/processing/parts_control/pc_data_ejb/Data.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tetrapak.processing.parts_control.pc_data_ejb;
+
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author SEPALMM
+ */
+@Remote
+public interface Data {
+
+    public String sayHello();
+
+    
+}
--- pc_data-ejb/src/main/java/com/tetrapak/processing/parts_control/pc_data_ejb/DataBean.java
+++ pc_data-ejb/src/main/java/com/tetrapak/processing/parts_control/pc_data_ejb/DataBean.java
@@ -5,20 +5,16 @@
 package com.tetrapak.processing.parts_control.pc_data_ejb;
 
 import javax.ejb.Stateless;
-import javax.ejb.LocalBean;
 
 /**
  *
  * @author Magnus Palm
  */
 @Stateless
-@LocalBean
-public class DataBean {
+public class DataBean implements Data {
 
-    public void businessMethod() {
+    @Override
+    public String sayHello() {
+        return ("Hello from DataBean");
     }
-
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
-
 }
