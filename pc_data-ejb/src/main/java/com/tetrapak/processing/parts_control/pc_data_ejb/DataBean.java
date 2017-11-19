1ec108285eace0db027cb66c00ba46b555e274c7
lmpanalytics2 <lmpconsulting@hotmail.com>
2017-11-19 10:02:05 +0100

    initial commit

---
+++ pc_data-ejb/src/main/java/com/tetrapak/processing/parts_control/pc_data_ejb/DataBean.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tetrapak.processing.parts_control.pc_data_ejb;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author Magnus Palm
+ */
+@Stateless
+@LocalBean
+public class DataBean {
+
+    public void businessMethod() {
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+}
---
+++ pc_logic-ejb/src/main/java/com/tetrapak/processing/parts_control/pc_logic_ejb/LogicBean.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tetrapak.processing.parts_control.pc_logic_ejb;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author Magnus Palm
+ */
+@Stateless
+@LocalBean
+public class LogicBean {
+
+    public void businessMethod() {
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+}
