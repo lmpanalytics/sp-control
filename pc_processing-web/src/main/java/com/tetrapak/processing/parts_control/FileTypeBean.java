1c4b79801909ddd2aa7b1c33d12030ec3d854638
lmpanalytics2 <lmpconsulting@hotmail.com>
2017-11-24 15:01:31 +0100

    Add selection functionality of xlsx file type to upload 	- Depending on type (Inventory, Purchases or Task list); there are different number of columns and etc

---
+++ pc_processing-web/src/main/java/com/tetrapak/processing/parts_control/FileTypeBean.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tetrapak.processing.parts_control;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ * Manages file type to upload
+ *
+ * @author SEPALMM
+ */
+@Named(value = "fileTypeBean")
+@RequestScoped
+public class FileTypeBean {
+
+    private String fileType;
+
+    /**
+     * Creates a new instance of FileTypeBean
+     */
+    public FileTypeBean() {
+    }
+
+    @PostConstruct
+    public void init() {
+//        System.out.println("I'm in FileTypeBean init() method");
+        // Initialize filetype
+        this.fileType = "";
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+//        System.out.printf("Selected file type: %s\n", this.fileType);
+    }
+
+}
